bf9063a1befff75a7bdd87c6c34d6df480f484b6
Eliot Jordan <eliotj@princeton.edu>
2024-07-23 22:44:59 -0500

    Updates to init script

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,3 +1,5 @@
 cp -R /project/workspace /project/gbl-app
 chown -R vscode:vscode /project/gbl-app
+cd /project/gbl-app
+git config --global --add safe.directory /project/gbl-app
 su vscode -c "bundle install || true && bundle exec rake geoblacklight:server"
