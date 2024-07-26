04e40f1e5a01e2bf76ab2a4bc81d80f0bef2bad7
Eliot Jordan <eliotj@princeton.edu>
2024-07-26 11:55:08 -0500

    Build docker image and push artifacts

--- template.rb
+++ template.rb
@@ -1,21 +1,12 @@
 # frozen_string_literal: true
 
 gem "blacklight", "~> 7.0"
-gem "sprockets", "< 4.0" # Use sprockets less than 4.0, let webpacker users set this up themselves
-
 if ENV["BRANCH"]
   gem "geoblacklight", github: "eliotjordan/geoblacklight", branch: ENV["BRANCH"]
 else
   gem "geoblacklight", "~> 4.0"
 end
-
-# Hack for https://github.com/rails/rails/issues/35153
-# Adapted from https://github.com/projectblacklight/blacklight/pull/2065
-gemfile = File.expand_path("Gemfile")
-File.write(gemfile, File.open(gemfile) do |f|
-  text = f.read
-  text.gsub(/^gem 'sqlite3'$/, 'gem "sqlite3", "~> 1.3.6"')
-end)
+gem "sprockets", "< 4.0" # Use sprockets less than 4.0, let webpacker users set this up themselves
 
 run "bundle install"
 
