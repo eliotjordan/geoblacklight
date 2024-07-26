4c022ecb63fef366016e2bc472173b5caa2e8373
Eliot Jordan <eliotj@princeton.edu>
2024-07-25 23:58:32 -0500

    Point template to forked repo

--- template.rb
+++ template.rb
@@ -4,7 +4,7 @@ gem "blacklight", "~> 7.0"
 gem "sprockets", "< 4.0" # Use sprockets less than 4.0, let webpacker users set this up themselves
 
 if ENV["BRANCH"]
-  gem "geoblacklight", github: "geoblacklight/geoblacklight", branch: ENV["BRANCH"]
+  gem "geoblacklight", github: "eliotjordan/geoblacklight", branch: ENV["BRANCH"]
 else
   gem "geoblacklight", "~> 4.0"
 end
